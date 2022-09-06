b6a99ef5657d74342452645fad5a5abaddb6d707
Scott-Gauthier <gauthier.scott@gmail.com>
2022-09-06 13:48:46 -0700

    Index.js now able to write readme.md files.

---
+++ files_created/test_1.md
@@ -0,0 +1,11 @@
+{
+	"title": "test 1",
+	"description": "asdf",
+	"table_of_contents": "asdf",
+	"installation": "asdf",
+	"usage": "afds",
+	"license": "adf",
+	"contributing": "afds",
+	"tests": "asdf",
+	"questions": "asdf"
+}
--- index.js
+++ index.js
@@ -1,13 +1,132 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require('inquirer');
+const fs = require('fs');
 // TODO: Create an array of questions for user input
-const questions = [];
-
+const questions = [
+    {
+        type: 'input',
+        message: 'What is your Project Title?',
+        name: 'title',
+    },
+    {
+        type: 'input',
+        message: 'What is your Project Description?',
+        name: 'description',
+    },
+    {
+        type: 'input',
+        message: 'What is your Project Table of Contents?',
+        name: 'table_of_contents',
+    },
+    {
+        type: 'input',
+        message: 'What is your Project Installation?',
+        name: 'installation',
+    },
+    {
+        type: 'input',
+        message: 'What is your Project Usage?',
+        name: 'usage',
+    },
+    {
+        type: 'input',
+        message: 'What is your Project License?',
+        name: 'license',
+    },
+    {
+        type: 'input',
+        message: 'What is your Project Contributing?',
+        name: 'contributing',
+    },
+    {
+        type: 'input',
+        message: 'What is your Project Tests?',
+        name: 'tests',
+    },
+    {
+        type: 'input',
+        message: 'What is your Project Questions?',
+        name: 'questions',
+    },
+];
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
-
+function writeToFile() {
+inquirer
+.prompt(questions)
+.then((data) => {
+    const filename = `./files_created/${data.title.toLowerCase().replace(' ', '_')}.md`;
+    fs.writeFile(filename, JSON.stringify(data, null, '\t'), (err) =>
+    err ? console.log(err) : console.log('Success!')
+    );
+  });
+};
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    writeToFile();
+};
 
 // Function call to initialize app
 init();
+
+
+
+
+// const inquirer = require('inquirer');
+// const fs = require('fs');
+// // TODO: Create an array of questions for user input
+
+// // TODO: Create a function to write README file
+// inquirer
+// .prompt([
+//     {
+//         type: 'input',
+//         message: 'What is your Project Title?',
+//         name: 'title',
+//     },
+//     {
+//         type: 'input',
+//         message: 'What is your Project Description?',
+//         name: 'description',
+//     },
+//     {
+//         type: 'input',
+//         message: 'What is your Project Table of Contents?',
+//         name: 'table_of_contents',
+//     },
+//     {
+//         type: 'input',
+//         message: 'What is your Project Installation?',
+//         name: 'installation',
+//     },
+//     {
+//         type: 'input',
+//         message: 'What is your Project Usage?',
+//         name: 'usage',
+//     },
+//     {
+//         type: 'input',
+//         message: 'What is your Project License?',
+//         name: 'license',
+//     },
+//     {
+//         type: 'input',
+//         message: 'What is your Project Contributing?',
+//         name: 'contributing',
+//     },
+//     {
+//         type: 'input',
+//         message: 'What is your Project Tests?',
+//         name: 'tests',
+//     },
+//     {
+//         type: 'input',
+//         message: 'What is your Project Questions?',
+//         name: 'questions',
+//     },
+// ])
+// .then((data) => {
+//     const filename = `${data.title.toLowerCase().replace(' ', '_')}.md`;
+//     fs.writeFile(filename, JSON.stringify(data, null, '\t'), (err) =>
+//     err ? console.log(err) : console.log('Success!')
+//     );
+//   });
